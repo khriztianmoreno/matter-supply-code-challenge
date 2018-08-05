5f49bdc9f20bd5095aa88d6e57cd8d387778aac8
khriztianmoreno <khriztianmoreno@gmail.com>
2018-08-04 22:08:54 -0500

    Get all the user's gits by default

--- src/pages/Home/Home.js
+++ src/pages/Home/Home.js
@@ -6,6 +6,10 @@
 import React, { Component } from 'react';
 import PropTypes from 'prop-types';
 
+import GistArticle from '../../components/GistArticle';
+
+import './Home.css';
+
 class HomePage extends Component {
   componentDidMount() {
     const { fetchAll } = this.props;
@@ -14,9 +18,16 @@ class HomePage extends Component {
   }
 
   render() {
+    const { gists } = this.props;
     return (
       <React.Fragment>
-        hola
+        <div className="main-container">
+          <div className="blog-posts">
+            {
+              gists && gists.map(item => <GistArticle gist={item} key={item.id} />)
+            }
+          </div>
+        </div>
       </React.Fragment>
     );
   }
@@ -24,6 +35,15 @@ class HomePage extends Component {
 
 HomePage.propTypes = {
   fetchAll: PropTypes.func.isRequired,
+  gists: PropTypes.arrayOf(PropTypes.shape({
+    id: PropTypes.string,
+    created_at: PropTypes.string,
+    description: PropTypes.string,
+    comments_url: PropTypes.string,
+    owner: PropTypes.shape({
+      html_url: PropTypes.string
+    })
+  })).isRequired
 };
 
 export default HomePage;
